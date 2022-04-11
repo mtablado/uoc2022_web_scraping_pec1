97d04232a67eeba6b122ff01bc2cafbd193c8f87
Miguel Tablado <migueltablado@gmail.com>
2022-04-11 18:24:52 +0200

    parse data

--- src/scrap.py
+++ src/scrap.py
@@ -1,18 +1,21 @@
 from bs4 import BeautifulSoup
 
 def scrap(html):
+    data = []
     soup = BeautifulSoup(html)
     items = soup.find_all('usgs-event-item-detail')
     for item in items:
-        print(item)
-        parse_item(item)
+        try:
+            item_data = parse_item(item)
+            data.append(item_data)
+        except:
+            print("Error parsing item:" + item)
+        
+    return data
 
 def parse_item(item):
     magnitude = item.find('span', {'class':'ng-star-inserted'}).text
     place = item.find('h6', {'class':'header'}).text
     time = item.find('span', {'class':'time'}).text
     depth = item.find('aside').find('span', {'class':'ng-star-inserted'}).text
-    print("Magnitude:" + magnitude)
-    print("Place:" + place)
-    print("Time:" + time)
-    print("Depth:" + depth)
+    return [time, place, depth, magnitude]
