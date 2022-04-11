404f7cd2081070161d85104f3de49ff9f71b25c9
Miguel Tablado <migueltablado@gmail.com>
2022-04-11 18:26:04 +0200

    process and save dataset

--- src/main.py
+++ src/main.py
@@ -1,14 +1,42 @@
 from web_download import selenium_download
 from scrap import scrap
 
+FILENAME = "dataset.csv"
+
+def create_csv_file(filename):
+    # Overwrite to the specified file.
+    # Create it if it does not exist.
+    header_line = "time;place;depth;magnitude"
+    file = open("../csv/" + filename, "w")
+    file.write(header_line)
+    file.write("\n")
+
+
+def append_csv(filename, data):
+    # Overwrite to the specified file.
+    # Create it if it does not exist.
+    file = open("../csv/" + filename, "a")
+
+    # Dump all the data with CSV format
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if (j == len(data[i])-1):
+                file.write(data[i][j])
+            else:
+                file.write(data[i][j] + ";")
+        file.write("\n")
+
 def main():
-    #html = download.selenium_download('https://www.ngdc.noaa.gov/hazel/view/hazards/earthquake/event-data')
-
-    html = selenium_download()
-    #html = download('https://www.ngdc.noaa.gov/hazel/view/hazards/earthquake/event-data')
-    #print(html)
-
-    scrap(html)
+    create_csv_file(FILENAME)
+    data = []
+    pages = 20
+    for page in range(1, pages):
+        html = selenium_download(page)
+        subset = scrap(html)
+        print(len(subset))
+        print(subset)
+        append_csv(FILENAME, subset)
+        data.append(subset)
 
 if __name__ == "__main__":
     main()
